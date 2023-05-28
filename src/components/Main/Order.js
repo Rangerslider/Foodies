d063a933e87316c4038f00ca59780d51a4d1c462
nuri <nuri1999sept@gmail.com>
2023-05-28 20:57:26 +0600

    Order Process Complete

--- src/components/Main/Order.js
+++ src/components/Main/Order.js
@@ -1,14 +1,64 @@
 import React from 'react';
 import '../../App.css';
-// import Menu from '../Menu';
-import Footer from '../Footer';
+// import Footer from '../Footer';
 
-function List() {
+// function List() {
+//   return (
+//     <>
+//       <Footer />
+//     </>
+//   );
+// }
+
+// export default List;
+
+
+import Main from '../Context/Main';
+import Basket from '../Context/Basket';
+import data from '../Context/Cdata';
+import { useState } from 'react';
+import '../Context/Cart.css'
+// import '../../App.css';
+
+function Order() {
+  const { products } = data;
+  const [cartItems, setCartItems] = useState([]);
+  const onAdd = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...product, qty: 1 }]);
+    }
+  };
+  const onRemove = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+        )
+      );
+    }
+  };
   return (
-    <>
-      <Footer />
-    </>
+    <div className="App">
+      <div className="row">
+        <Main products={products} onAdd={onAdd}></Main>
+        <Basket
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+        ></Basket>
+      </div>
+    </div>
   );
 }
 
-export default List;
+export default Order;
