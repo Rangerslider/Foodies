494d40932261374a6f6d309758ebc55af234d50e
nuri <nuri1999sept@gmail.com>
2023-07-03 21:30:18 +0600

    home page design

--- src/App.js
+++ src/App.js
@@ -1,30 +1,7 @@
-import React from 'react';
-import Navbar from './components/Navbar.js';
-import './App.css';
-import Home from './components/Main/Home.js';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import List from './components/Main/List.js';
-import Order from './components/Main/Order.js';
-import Contact from './components/Main/Contact.js';
-
-
-
-
+import Layout from "./components/Layout/Layout";
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/list' component={List} />
-          <Route path='/Order' component={Order} />
-          <Route path='/contact' component={Contact} /> 
-        </Switch>
-      </Router>
-    </>
-  );
+  return <Layout />;
 }
 
 export default App;
