3a81c4a03def852dc9563f34de9265a5393c8f42
nuri <nuri1999sept@gmail.com>
2023-07-18 19:45:53 +0600

    Update all css for ui

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import "../styles/contact.css";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <>
+      <section class="contact" id="contact">
+        <div class="row">
+          <form action="">
+            <h3>get in touch</h3>
+            <div class="inputBox">
+              <span class="fas fa-user"></span>
+              <input type="text" placeholder="name" />
+            </div>
+            <div class="inputBox">
+              <span class="fas fa-envelope"></span>
+              <input type="email" placeholder="email" />
+            </div>
+            <div class="inputBox">
+              <span class="fas fa-phone"></span>
+              <input type="number" placeholder="number" />
+            </div>
+            <input type="submit" value="contact now" class="btn" />
+          </form>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Contact;
